e7b185d68777440db75bebd76a3cb769bfba4955
Ubuntu <ubuntu@ip-172-31-9-8.ec2.internal>
2020-03-28 08:58:32 +0000

    clean

--- ch3/calc_rmse_three_models.jl
+++ ch3/calc_rmse_three_models.jl
@@ -4,11 +4,13 @@ using Distributions
 using LinearAlgebra
 using ProgressBars
 
+# if use ssh connection, comment out
 ENV["GKSwstype"]="100"
 
 ######### sample point creation #########
 
 # make ground truth
+# to check -> https://github.com/triwave33/julia_bayes_ml_suyama/blob/master/ch3/fig_3_10.png
 function truth(x, a, center)
   return (1 + sin(x)) + (a*(x - center)^2)
 end
@@ -39,8 +41,7 @@ function get_feature_matrix(x_arr, M)
 end
 
 # regression
-function pred_poly_reg!(λ, x_arr, y_arr, x_grid, M; isPlot, label)
-  
+function pred_poly_reg(λ, x_arr, y_arr, x_grid, M; isPlot, label)
   m = zeros(M)
   Λ = I(M)
 
@@ -49,14 +50,7 @@ function pred_poly_reg!(λ, x_arr, y_arr, x_grid, M; isPlot, label)
   m̂ = inv(Λ̂)*(λ* f_arr * y_arr' +Λ* m)
   f_grid = get_feature_matrix(x_grid,M) # feature
   μ_post = f_grid' * m̂ # y_pred
-  λ_post_inv = inv(λ) .+ diag(f_grid' * inv(Λ̂)*  f_grid)
-  p = :none
-  if isPlot == true
-    # plot iference results
-    p= plot!(x_grid, μ_post[:,1],legend=true, linewidth=1, label=label)
-  end
-
-  return p, μ_post
+  return μ_post
 end
 ############################################
 
@@ -71,111 +65,40 @@ function get_NN(x_ref_arr, y_ref_arr, x)
 end
 
 # regression
-function pred_NN_reg!(x_arr, y_arr, x_grid; isPlot)
+function pred_NN_reg(x_arr, y_arr, x_grid; isPlot)
   y_pred = get_NN.(Ref(x_arr), Ref(y_arr), x_grid)
-  p = :none
-  if isPlot == true
-    p = plot!(x_grid, y_pred, label="nn")
-  end
-  return p, y_pred
+  return y_pred
 end
 ######################################
 
 
-
-# plot all regresion results for fig_3_10
-function plot_all_regs!(x_grid, y_grid, x_sample, y_sample,  N; isPlot, kwargs...)
-  λ = 13.0
-  a = 1.0/40
-  center = 2
-
-  p = plot()
-  p = plot!(x_grid, y_grid, label="truth"; kwargs...)
-  p, _ = pred_poly_reg!(λ, x_sample, y_sample, x_grid, 2; isPlot=isPlot, label="line")
-  p, _ = pred_poly_reg!(λ, x_sample, y_sample, x_grid, 3; isPlot=isPlot, label="quad")
-  p, _  = pred_NN_reg!(x_sample, y_sample, x_grid; isPlot=isPlot)
-  p = plot!(legend=:topleft, legendfont= font(5),
-            xlim=(-10,30), ylim=(-5,20))
-  p = plot!(x_sample, y_sample', seriestype=:scatter, 
-            label="data", markerstrokecolor=false, 
-            markercolor=:purple, markersize=3)
-  p = plot!(annotations=[(-5,18, text("N=$N",8))])
-
-  return p
-end
-
-# calculate root mean square error for fig 3.11
+# calculate root mean square error
 function calc_rmse(y_pred, y_ans)
   return sqrt(mean((y_pred - y_ans).^2))
 end
 
 
-###############################################################
-
-
-function fig_3_10()
-  a = 1.0/40
-  center=2.0
-  λ = 13.0 
-  
-  # make ground truth
-  x_min = -10
-  x_max = 30
-  x_step = 0.01
-  x_grid = x_min:x_step:x_max
-  y_grid = truth.(x_grid, a, center)
-  
-  
-  #y_arr = reshape(rand.(Normal.(sin.(x_arr), λ^(-1))), (N, 1))'
-  l = @layout [a; b c]
-  m = Dict(:left_margin=>40mm, :right_margin=>40mm)
-  
-  N_arr = [3,10,50]
-  PP = []
-  for (i,n) in enumerate(N_arr)
-    # make sampling data points
-    x_sample, y_sample = get_sample(n, x_min, x_max, a, center)
-  
-    plot
-    p = plot()
-    if i ==1
-      p = plot_all_regs!(x_grid, y_grid, x_sample, y_sample, n; isPlot=true, m...)
-    else
-      p = plot_all_regs!(x_grid, y_grid, x_sample, y_sample, n; isPlot=true)
-    end
-    push!(PP, p)
-  end
-  plot(PP..., dpi=600, layout=l)
-  savefig("fig_3_10.png")
-end
-
-
-
-############# fig_3_11.jl ##############
-function fig_3_11()
+function main()
   N_train = 1000 # maximum num of sampling points 
   N_test = 20 # sample points for calculate RMSE
   N_train_arr = 1:N_train 
   N_trial = 10 # averaging num for regression results (RMSE)
 
+  # parameter for ground truth
+  a = 1.0/40 # coef for quadratic func
+  center=2.0 # center point of quadratic func
+  λ = 13.0 # noise parameter
   x_min = -10
   x_max = 30
   x_step = 0.01
-
- 
+  
+  x_test, y_test = get_sample(N_test, x_min, x_max, a, center)
+  
+  # initialize
   lin_rmse_arr = zeros(N_train)
   quad_rmse_arr = zeros(N_train)
   nn_rmse_arr = zeros(N_train)
-
-  a = 1.0/40
-  center=2.0
-  λ = 13.0
-  x_min = -10
-  x_max = 30
-  x_step = 0.01
-
- 
-  x_test, y_test = get_sample(N_test, x_min, x_max, a, center)
+  
   for n in ProgressBar(N_train_arr) # increasing sample points
     lin_tmp = 0 
     quad_tmp = 0
@@ -184,15 +107,15 @@ function fig_3_11()
       x_train, y_train = get_sample(n, x_min, x_max, a, center)
     
       # linear
-      _, y_pred = pred_poly_reg!(λ, x_train, y_train, x_test, 2;
+      y_pred = pred_poly_reg(λ, x_train, y_train, x_test, 2;
                                   isPlot=false, label="line")
       lin_tmp += calc_rmse(y_test, y_pred')
       # quad
-      _, y_pred = pred_poly_reg!(λ, x_train, y_train, x_test, 3;
+      y_pred = pred_poly_reg(λ, x_train, y_train, x_test, 3;
                                   isPlot=false, label="quad")
       quad_tmp += calc_rmse(y_test, y_pred')
       # NN
-      _, y_pred  = pred_NN_reg!(x_train, y_train, x_test;
+      y_pred  = pred_NN_reg(x_train, y_train, x_test;
                                   isPlot=false)
       nn_tmp += calc_rmse(y_test, y_pred')
     end
@@ -203,14 +126,13 @@ function fig_3_11()
   end
     
     
-  plot()
+  plot(xlabel="sample_num", ylabel="rmse")
   plot!(log10.(lin_rmse_arr), xaxis=:log,  label="lin")
   plot!(log10.(quad_rmse_arr), xaxis=:log,  label="quad")
   plot!(log10.(nn_rmse_arr), xaxis=:log,  label="nn")
-  savefig("fig_3_11.png")
+  savefig("rmse.png")
 end
 
-fig_3_10()
-println("running fig_3_11")
-fig_3_11()
+println("running main")
+main()
 
